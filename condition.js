a112cb3d84f605ed8cdaa7131edc232ce3632127
sabermahmud <sabermahmud01@gmail.com>
2026-07-16 05:56:22 +0300

    check with if else. is this number positive or negative or zero

---
+++ condition.js
@@ -0,0 +1,11 @@
+// একটি সংখ্যা positive, negative, নাকি zero—তা বের করো।
+let number = 0;
+if(number > 0 ){
+    console.log("this number is positive")
+}
+else if(number === 0){
+    console.log("it's zero")
+}
+else{
+    console.log("this is negative number")
+}
